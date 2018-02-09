6fb2614baf0f78c8846700d690065ce725a0adf5
张 骞 <zq_sdk@163.com>
2018-02-09 10:57:49 +0800

    AES

---
+++ common/AESEncrypt.js
@@ -0,0 +1,33 @@
+const crypto = require('crypto')
+
+class AESEncrypt {
+
+  assembleCode(data, key = '', iv = '', operation) {
+    if (!data) return null
+
+    try {
+      switch (operation) {
+        case 'decrypt':
+          const decipher = crypto.createDecipheriv('aes-128-cbc', key, new Buffer(iv, 'utf-8'))
+          return decipher.update(data, 'hex', 'utf8') + decipher.final('utf8')
+        case 'encrypt':
+          const cipher = crypto.createCipheriv('aes-128-cbc', key, new Buffer(iv, 'utf-8'))
+          return cipher.update(data, 'utf8', 'hex') + cipher.final('hex')
+      }
+    } catch (err) {
+      sails.log.error(err)
+      return null
+    }
+  }
+
+  encrypt(str, key, iv) {
+    return this.assembleCode(str, key, iv, 'encrypt')
+  }
+
+  decrypt(str, key, iv) {
+    return this.assembleCode(str, key, iv, 'decrypt')
+  }
+
+}
+
+module.exports = new AESEncrypt()
