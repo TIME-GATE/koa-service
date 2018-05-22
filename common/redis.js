9953ebfa6947bcbca24be494e613c4dcd2c76aa6
张 骞 <zq_sdk@163.com>
2018-05-22 16:43:38 +0800

    redis

--- common/redis.js
+++ common/redis.js
@@ -1,6 +1,11 @@
 const config = require('../config')
 const redis = require('redis')
-const redisClient = redis.createClient(config.database.redis.port, config.database.redis.host, config.database.redis.options)
+
+const redisClient = redis.createClient(
+  config.database.redis.port, 
+  config.database.redis.host, 
+  config.database.redis.options
+)
 
 module.exports = redisClient
 
